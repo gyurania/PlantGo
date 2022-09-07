43badc1ce006b1fda8b4b2c3ea170b55a0f1b372
이재익 <csljoke@gmail.com>
2022-09-07 16:37:35 +0900

    FE_feat : Router 추가

--- plantgo/src/index.tsx
+++ plantgo/src/index.tsx
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux/es/exports';
-import store from './store';
+import store from './store/index';
+import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>
+  <BrowserRouter>
+    <Provider store={store()}>
+        <App />
+    </Provider>
+  </BrowserRouter>
 );
