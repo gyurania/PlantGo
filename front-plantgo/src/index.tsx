5c45236954b5bafda2e33ca73e28264bb56b598b
이재익 <csljoke@naver.com>
2022-09-15 11:04:00 +0900

    new: 이름 변경

---
+++ front-plantgo/src/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Login from './views/loginView'
+import Home from './views/homeView'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
+// import { Provider } from 'react-redux';
+import './index.css';
+import axios from 'axios';
+import SuccessLoginKaKao from './components/successLoginKakao';
+
+const container = document.getElementById('root')!;
+const root = createRoot(container);
+axios.defaults.withCredentials = true;
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/login' element={<Login />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/oauth/callback/kakao' element={<SuccessLoginKaKao/>}/>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
---
+++ front-plantgo/src/informations/oauth.ts
@@ -0,0 +1,8 @@
+import CLIENT_ID from "./code";
+
+const BACK_URI = "http://localhost:8080/oauth2/authorization";
+const REDIRECT_URI = "http://localhost:3000/oauth/redirect";
+
+export const KAKAO_AUTH_URL = `${BACK_URI}/kakao?redirect_uri=${REDIRECT_URI}`;
+export const NAVER_AUTH_URL = `${BACK_URI}/naver?redirect_uri=${REDIRECT_URI}`;
+export const GOOGLE_AUTH_URL = `${BACK_URI}/google?redirect_uri=${REDIRECT_URI}`;
