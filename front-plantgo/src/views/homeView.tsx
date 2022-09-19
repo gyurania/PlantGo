837ebce0ea7fcba229793310314dc3d6aa96a46e
vvchldmsdn@google.com <vvchldmsdn@gmail.com>
2022-09-19 09:00:25 +0900

    feat: homeview naver map script

--- front-plantgo/src/views/homeView.tsx
+++ front-plantgo/src/views/homeView.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import './HomeView.css'
+import "./HomeView.css";
 
 import NaverMap from "../components/NaverMap";
 
@@ -9,47 +9,59 @@ function HomeView() {
   //   window.location.replace("/login")
   // }
 
-  const [latitude, setLatitude] = useState(37.5656)
-  const [longitude, setLongitude] = useState(126.9769)
-  const [nearPlants, setNearPlants] = useState([])  // {lat, lng, plantNm, plantImg}
+  const [latitude, setLatitude] = useState(37.5656);
+  const [longitude, setLongitude] = useState(126.9769);
+  const [nearPlants, setNearPlants] = useState([]); // {lat, lng, plantNm, plantImg}
+  const [isRenewed, setIsRenewed] = useState(Boolean);
 
   // 1. 현재 위치 가져오기
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((position) => {
-      setLatitude(position.coords.latitude)
-      setLongitude(position.coords.longitude)
-      console.log(position)
-    })
+      setLatitude(position.coords.latitude);
+      setLongitude(position.coords.longitude);
+      console.log(position);
+    });
   } else {
-    window.alert("현재위치를 확인할 수 없습니다ㅠㅠ")
+    window.alert("현재위치를 확인할 수 없습니다ㅠㅠ");
   }
 
   // 2. 현재 위치 주변 식물 정보 받기 비동기처리
   useEffect(() => {
     axios({
-      method: 'get',
-      url: 'http://localhost:8080/api/map',
+      method: "get",
+      url: "http://localhost:8080/api/map",
       data: {
         lat: latitude,
-        lng: longitude
-      }
+        lng: longitude,
+      },
     })
       .then((res) => {
-        setNearPlants(res.data)
+        setNearPlants(res.data);
       })
-      .catch((err) => console.log(err))
-  }, [latitude, longitude])
+      .catch((err) => console.log(err));
+  }, [latitude, longitude]);
+
+  // const function = getIsRenewed(isRenewed:Boolean) => {
+  //   setIsRenewed((isRenewed + 1) % 2)
+  // }
 
   return (
     <div>
       <div className="over-map">
         <h2 className="title">Plant Go!</h2>
         <p className="plus-icon">Plus Button</p>
+        <p className="plus-icon">
+          Renew Button
+        </p>
       </div>
-      <NaverMap lat={latitude} lng={longitude} nearPlants={nearPlants} />
+      <NaverMap
+        lat={latitude}
+        lng={longitude}
+        nearPlants={nearPlants}
+        renew={isRenewed}
+      />
     </div>
-  )
+  );
 }
-
 
 export default HomeView;
