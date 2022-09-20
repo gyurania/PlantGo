6d8fbb175e2b55693a7370c0fe9a200fce5a134e
이재익 <csljoke@gmail.com>
2022-09-20 18:08:19 +0900

    feat: 로그인 화면 구성요소 추가

--- front-plantgo/src/views/loginView.tsx
+++ front-plantgo/src/views/loginView.tsx
@@ -2,10 +2,20 @@ import React from "react";
 import GoogleLogin from "../components/Google";
 import NaverLogin from "../components/Naver";
 import KakaoLogin from "../components/Kakao";
+import bgimg from "../img/loginBackground.png";
+import logo from "../img/plantgo-white-font.png";
 
 function Login() {
   return (
-    <div>
+    <div style={{
+      backgroundImage: `url(${bgimg})`,
+      width: 360,
+      height: 800
+      }}>
+      <img src={logo} alt="" style={{
+        width: 250,
+        height: 350,
+      }}/>
       <KakaoLogin />
       <br />
       <GoogleLogin />
