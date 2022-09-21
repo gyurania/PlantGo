f499080d9135a3c2683363b691d33a84a209bea9
vvchldmsdn@google.com <vvchldmsdn@gmail.com>
2022-09-21 17:45:38 +0900

    feat: plantresult loading

---
+++ front-plantgo/src/views/cameraView.jsx
@@ -0,0 +1,116 @@
+import axios from "axios";
+import React, { useEffect, useState, useRef } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import Webcam from "react-webcam";
+// import {
+//   RemoveBgResult,
+//   RemoveBgError,
+//   removeBackgroundFromImageBase64,
+// } from "remove.bg";
+
+const videoConstraints = {
+  width: 360,
+  height: 500,
+  facingMode: "user",
+};
+
+const WebcamCapture = () => {
+  const webcamRef = useRef(null);
+
+  const [imgSrc, setImgSrc] = useState("");
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (imgSrc !== "") {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+          // setLatitude(position.coords.latitude);
+          // setLongitude(position.coords.longitude);
+          axios({
+            method: "get",
+            url: `/map-reversegeocode/v2/gc?coords=${position.coords.longitude},${position.coords.latitude}&output=json`,
+            headers: {
+              "X-NCP-APIGW-API-KEY-ID": "6s70rnjtot",
+              "X-NCP-APIGW-API-KEY": "uDvd8ChhbkZbYjXX1z7y88hd3bZEiLEzYtN8kiiq",
+            },
+          }).then((res) => {
+            console.log(
+              "reverse geocode",
+              res.data.results[0].region.area2.name
+            );
+            navigate("/plantResult", {
+              state: {
+                imgSrc: imgSrc,
+                position: {
+                  lat: position.coords.latitude,
+                  lng: position.coords.longitude,
+                },
+                area: res.data.results[0].region.area2.name,
+              },
+            });
+          });
+        });
+      } else {
+        window.alert("현재 위치를 확인할 수 없습니다ㅠㅠ");
+      }
+    }
+  }, [imgSrc]);
+
+  const capture = React.useCallback(() => {
+    const imageSrc = webcamRef.current?.getScreenshot();
+    if (imageSrc) {
+      setImgSrc(imageSrc);
+    }
+  }, [webcamRef]);
+
+  return (
+    <>
+      <Webcam
+        audio={false}
+        ref={webcamRef}
+        screenshotFormat="image/jpeg"
+        width="360px"
+        height="600px"
+        videoConstraints={videoConstraints}
+      />
+      <button onClick={capture}>Capture photo</button>
+      {/* { imgSrc && (
+        <img
+          src={ imgSrc }
+        />
+      ) } */}
+    </>
+  );
+};
+
+export default WebcamCapture;
+// const videoConstraints = {
+//   width: 360,
+//   height: 600,
+//   facingMode: "user",
+// };
+
+// const WebcamCapture = () => {
+//   return (
+//     <div>
+//       <Webcam
+//         audio={false}
+//         height={600}
+//         screenshotFormat="image/jpeg"
+//         width={360}
+//         videoConstraints={videoConstraints}
+//       >
+//         {({ getScreenshot }) => {
+//           <button
+//             onClick={() => {
+//               const imageSrc = getScreenshot()
+//             }}
+//           >Capture Photo</button>;
+//         }}
+//       </Webcam>
+//     </div>
+//   );
+// };
+
+// export default WebcamCapture;
--- front-plantgo/src/views/plantResultView.tsx
+++ front-plantgo/src/views/plantResultView.tsx
@@ -1,26 +1,55 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
-// import {
-//   RemoveBgResult,
-//   RemoveBgError,
-//   removeBackgroundFromImageBase64,
-// } from "remove.bg";
+import {
+  RemoveBgResult,
+  RemoveBgError,
+  removeBackgroundFromImageFile,
+} from "remove.bg";
 // import * as fs from "fs";
 
 function PlantResultView() {
   const location = useLocation();
 
+  const [loading, setLoading] = useState<boolean>(true);
+  const [info, setInfo] = useState<Object>({});
+  const token = sessionStorage.getItem("token");
   // const localFile = "/public/monstera.jpg";
-  // const base64img = fs.readFileSync(localFile, { encoding: "base64" });
-  // console.log(typeof base64img);
 
-  // removeBackgroundFromImageBase64({
-  //   base64img,
+  useEffect(() => {
+    if (loading) {
+      axios({
+        method: "post",
+        url: "http://j7a703.p.ssafy.io:8080/api/photocard",
+        headers: {
+          Authorization: `'Bearer ' + ${token}`,
+        },
+        data: {
+          img: location.state.imgSrc,
+          latitude: location.state.position.lat,
+          longitude: location.state.position.lng,
+          area: location.state.area,
+        },
+      })
+        .then((res) => {
+          setLoading(false);
+          setInfo(res.data);
+        })
+        .catch((err) => console.log(err));
+    } else {
+      window.alert("잘못 된 접근입니다.");
+    }
+  }, []);
+
+  // removeBackgroundFromImageFile({
+  //   path: localFile,
   //   apiKey: "9tRSQ11xfpwdrxkXF3hWcjSM",
   //   size: "regular",
-  //   type: "product",
+  //   type: "auto",
+  //   scale: "50%",
   // }).then((res: RemoveBgResult) => {
   //   const base64img = res.base64img;
+  //   console.log(base64img);
   // });
 
   return (
