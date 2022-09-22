cb21a84e61fa4bf6a823974ec0b4801f229d899d
Inyoung.R <inyoung.r@gmail.com>
2022-09-22 15:18:41 +0900

    feat: spark mysql connected

--- plantgo-spark2/src/main/java/SimpleApp.java
+++ plantgo-spark2/src/main/java/SimpleApp.java
@@ -1,17 +1,37 @@
 /* SimpleApp.java */
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SQLContext;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.Dataset;
+import static org.apache.spark.sql.functions.col;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
 
 public class SimpleApp {
+
+    private static final String MYSQL_DRIVER = "com.mysql.jdbc.Driver";
+    private static final String MYSQL_CONNECTION_URL = "jdbc:mysql://j7a703.p.ssafy.io:3306/plantgo?allowPublicKeyRetrieval=true&useSSL=false";
+    private static final String MYSQL_DBtable = "plant";
+    private static final String MYSQL_USERNAME = "plantgo";
+    private static final String MYSQL_PWD = "a703pg7";
+
     public static void main(String[] args) {
         String logFile = "/usr/lib/spark/README.md"; // Should be some file on your system
         SparkSession spark = SparkSession.builder().appName("Simple Application").config("spark.master", "local").getOrCreate();
-        Dataset<String> logData = spark.read().textFile(logFile).cache();
+        //Dataset<String> logData = spark.read().textFile(logFile).cache();
 
-        long numAs = logData.filter(s -> s.contains("a")).count();
-        long numBs = logData.filter(s -> s.contains("b")).count();
+        Properties connectionProperties = new Properties();
+        connectionProperties.put("user", MYSQL_USERNAME);
+        connectionProperties.put("password", MYSQL_PWD);
+        Dataset<Row> jdbcDF2 = spark.read()
+                .jdbc(MYSQL_CONNECTION_URL, MYSQL_DBtable, connectionProperties);
+        //long numAs = logData.filter(s -> s.contains("a")).count();
+        //long numBs = logData.filter(s -> s.contains("b")).count();
 
-        System.out.println("Lines with a: " + numAs + ", lines with b: " + numBs);
+        //jdbcDF2.filter(col("kor_name").equals("회색사초")).show();
 
         spark.stop();
     }
