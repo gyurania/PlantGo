5371a20aa38bccb5b3ef74fdfe5a11025d805853
이재익 <csljoke@naver.com>
2022-09-26 16:56:55 +0900

    지겹당... 무한스크롤..

--- front-plantgo/src/views/plantsListView.tsx
+++ front-plantgo/src/views/plantsListView.tsx
@@ -1,19 +1,19 @@
 import React, { useEffect, useState, useRef } from "react";
 import axios from "axios";
 import spring from "../api/spring";
-import { useInView } from "react-intersection-observer"
 
 
 function PlantList() {
 
+  const TOTAL_PAGES = 419;
 
   // useState, useRef, useInview
-  
   const [userSeq, setUserSeq] = useState(0);
   const [plantList, setPlantList] = useState<any>([]);
   const [endPage, setEndPage] = useState<boolean>(true)
   const [page, setPage] = useState<number>(1);
-  const [ref, inView] = useInView();
+  const [loading, setLoading] = useState<boolean>(true)
+  const [lastElement, setLastElement] = useState<any>(null);
   // const [collectedPlantList, setCollectedPlantList] = useState([]);
   // const [nonCollectedPlantList, setNonCollectedPlantList] = useState([]);
 
@@ -27,6 +27,19 @@ function PlantList() {
     window.location.replace('/login')
   }
 
+  // observer
+
+  const observer = useRef(
+    new IntersectionObserver(
+        (entries) => {
+            const first = entries[0];
+            if (first.isIntersecting) {
+                setPage((no) => no + 1);
+            }
+        })
+  );
+
+  // userSeq get
   const getUserSeq = () => {
     axios({
       method: 'get',
@@ -44,8 +57,10 @@ function PlantList() {
       })
   }
   
-  // plantlist 가져오는 함수
+  // plantlist
   const fetchPlantList = () => {
+    
+    setLoading(true)
     axios({
       method: 'post',
       url: spring.plants.list(),
@@ -58,15 +73,11 @@ function PlantList() {
       }
     })
     .then((res) => {
+      let all:any = new Set([...plantList, ...res.data.plantDtoList]);
       console.log(res.data)
-      setPlantList((plantList: any) => [...plantList, ...res.data.plantDtoList])
-      if (page === 419) {
-        setEndPage(false)
-      }
-      else {
-        setPage(prev=>prev+1)
-      }
+      setPlantList([...all]);
       console.log(page)
+      setLoading(false);
     })
     .catch((err) => {
       console.error(err)
@@ -117,35 +128,77 @@ function PlantList() {
 
   // 곧바로 실행되는 것(최초 1회)
   useEffect(() => {
-    // userSeq 가져오는 함수
-    getUserSeq()
-  }, [])
-
-  // pageNumber에 변화 있으면 실행
-  useEffect(() => {
-    console.log(inView, endPage);
-    if (inView && endPage) {
-      fetchPlantList();
+    if (page <= TOTAL_PAGES) {
+        fetchPlantList();
     }
-  }, [page, inView]);
+  }, [page]);
+  
+  useEffect(() => {
+    const currentElement = lastElement;
+    const currentObserver = observer.current;
 
-  return (
-    <div style={{
-      width: 360,
-      height: 800
-    }}>
-      <br/>
-      <div style={{ position: 'relative' }}>
-        {plantList.map((plant: any) => {
-          <ul key={plant.plantId}>
-            <li>{plant.korName}</li>
-            <li>{plant.imgUrl}</li>
-          </ul>
-        })};
+    if (currentElement) {
+        currentObserver.observe(currentElement);
+    }
+
+    return () => {
+        if (currentElement) {
+            currentObserver.unobserve(currentElement);
+        }
+    };
+  }, [lastElement]);
+
+  const UserCard = (data:any) => {
+    return (
+        <div className='p-4 border border-gray-500 rounded bg-white flex items-center'>
+            <div>
+                <img
+                    src={data.imgUrl}
+                    className='w-16 h-16 rounded-full border-2 border-green-600'
+                    alt='user'
+                />
+            </div>
+
+            <div className='ml-3'>
+                <p className='text-base font-bold'>
+                  {data.korName}
+                </p>
+            </div>
+        </div>
+      );
+  };
+  
+    return (
+      <div className='mx-44 bg-gray-100 p-6'>
+          <h1 className='text-3xl text-center mt-4 mb-10'>All Plants</h1>
+
+          <div className='grid grid-cols-3 gap-4'>
+              {plantList.length > 0 &&
+                  plantList.map((plant:any, i:number) => {
+                      return i === plantList.length - 1 &&
+                          !loading &&
+                          page <= TOTAL_PAGES ? (
+                          <div
+                              key={`${plantList.korName}-${i}`}
+                              ref={setLastElement}
+                          >
+                              <UserCard data={plant} />
+                          </div>
+                      ) : (
+                          <UserCard
+                              data={plant}
+                              key={`${plantList.korName}-${i}`}
+                          />
+                      );
+                  })}
+          </div>
+          {loading && <p className='text-center'>loading...</p>}
+
+          {page - 1 === TOTAL_PAGES && (
+              <p className='text-center my-10'>♥</p>
+          )}
       </div>
-      <div ref={ref} style={{ position: 'absolute', bottom: '100px' }} />
-    </div>
-  )
+  );
 }
       
 export default PlantList;
