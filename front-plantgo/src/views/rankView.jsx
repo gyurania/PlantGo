d1d8bdc12ab8c1bd43901a7970a1ae28acddbe7b
배현중 <alex950107@gmail.com>
2022-09-26 13:37:58 +0900

    fix: rank

--- front-plantgo/src/views/rankView.jsx
+++ front-plantgo/src/views/rankView.jsx
@@ -5,25 +5,33 @@ import { useNavigate, Link } from "react-router-dom";
 const Ranking = () => {
   const navigate = useNavigate();
   const [rank, setRank] = useState(null);
+  const [rendered, setRendered] = useState(0);
   //   const token = sessionStorage.getItem("loginToken");
   const token =
     "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIyNDM1Nzg1MzAxIiwicm9sZSI6IlJPTEVfVVNFUiIsImV4cCI6MTY2NDE4MzAwOH0.FDgjXXMEDjfRqZLS6RIgHMQpLK0alv0rKrLmju4PGrM";
+
+  useEffect(() => {
+    setRendered(1);
+  }, []);
+
   // 백엔드에서 rank data 받아오기
   useEffect(() => {
-    axios({
-      method: "get",
-      url: "https://j7a703.p.ssafy.io/api/v1/users/rank",
-      headers: {
-        Authorization: `Bearer ${token}`,
-        "Access-Control-Allow-Credentials": true,
-        "Content-Type": "aaplication/json",
-      },
-    })
-      .then((res) => {
-        setRank(res.data);
+    if (rendered === 1) {
+      axios({
+        method: "get",
+        url: "https://j7a703.p.ssafy.io/api/v1/users/rank",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Access-Control-Allow-Credentials": true,
+          "Content-Type": "aaplication/json",
+        },
       })
-      .catch((err) => console.log(err));
-  }, []);
+        .then((res) => {
+          setRank(res.data);
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [rendered]);
 
   useEffect(() => {
     console.log("rank는???");
@@ -33,6 +41,8 @@ const Ranking = () => {
   return (
     <div>
       <div>{JSON.stringify(rank)}</div>
+      <h2> 아 몰랑 </h2>
+      <div>{rank.rankList[0]}</div>
     </div>
   );
 };
