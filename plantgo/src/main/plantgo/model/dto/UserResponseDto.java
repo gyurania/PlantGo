bdf36fcae69607e0345be9b3183064c7316b2b9e
이재익 <csljoke@naver.com>
2022-09-26 11:37:01 +0900

    git 변경

---
+++ plantgo/src/main/plantgo/model/dto/UserResponseDto.java
@@ -0,0 +1,16 @@
+package com.ssafy.plantgo.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserResponseDto {
+
+    String username;
+    String email;
+}
