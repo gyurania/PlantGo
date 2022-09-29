7cd0c5896a729560bee7bbe21d5c844f68f5a39c
배현중 <alex950107@gmail.com>
2022-09-30 08:43:39 +0900

    fix: gitignore

---
+++ front-plantgo/src/informations/oauth.ts
@@ -0,0 +1,10 @@
+const BACK_URI = "http://j7a703.p.ssafy.io:8080/oauth2/authorization";
+const REDIRECT_URI = "http://localhost:3000/oauth/redirect";
+
+export const KAKAO_AUTH_URL = `${BACK_URI}/kakao?redirect_uri=${REDIRECT_URI}`;
+export const NAVER_AUTH_URL = `${BACK_URI}/naver?redirect_uri=${REDIRECT_URI}`;
+export const GOOGLE_AUTH_URL = `${BACK_URI}/google?redirect_uri=${REDIRECT_URI}`;
+
+// 바뀌는거 안보이겠지?
+// 왜 보이는거지?
+// 캐시 삭제후 재시도
