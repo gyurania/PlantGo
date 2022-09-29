86a6e294d67a7e2f92ddc4562a19d3a745a2f66c
배현중 <alex950107@gmail.com>
2022-09-30 08:30:54 +0900

    fix: gitignore

--- front-plantgo/src/informations/oauth.ts
+++ front-plantgo/src/informations/oauth.ts
@@ -4,3 +4,5 @@ const REDIRECT_URI = "http://localhost:3000/oauth/redirect";
 export const KAKAO_AUTH_URL = `${BACK_URI}/kakao?redirect_uri=${REDIRECT_URI}`;
 export const NAVER_AUTH_URL = `${BACK_URI}/naver?redirect_uri=${REDIRECT_URI}`;
 export const GOOGLE_AUTH_URL = `${BACK_URI}/google?redirect_uri=${REDIRECT_URI}`;
+
+// 바뀌는거 안보이겠지?
