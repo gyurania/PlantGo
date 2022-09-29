f5bcae5082265ddd2be9dc32ecda9bbdde9633ad
김규란 <gyuraniakim@naver.com>
2022-09-30 07:20:54 +0900

    design: rank 페이지 디자인 수정 중

--- front-plantgo/src/views/rankView.jsx
+++ front-plantgo/src/views/rankView.jsx
@@ -12,6 +12,10 @@ import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import { useNavigate, Link } from "react-router-dom";
 import "./RankView.css";
+import "./ranktemp.css";
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 
 const Ranking = () => {
   const navigate = useNavigate();
@@ -100,7 +104,258 @@ const Ranking = () => {
       // result.push(</List>);
       return result;
     };
-    return <div>{rendering()}</div>;
+    return (
+      <Container fluid className="backgroundImg">
+        <script>
+          var sum = 1+2+3+4; document.getElementById("name").innerHTML=sum;
+        </script>
+        <div>
+          {/* {rendering()} */}
+          <body class="page-leaderboard">
+            <div class="rank-title">
+              <img
+                alt="Android Basics Leaderboard"
+                class="mb-2"
+                src="https://d125fmws0bore1.cloudfront.net/assets/svgs/icon_trophy_leaderboard-3442a4b2312e6cdd02aa9870e636dc082890277a6267c4ed986a750fef7cbb35.svg"
+              />
+              <br />
+              Plant Go Ranking
+            </div>
+            <section class="ranking">
+              <div class="contain">
+                <div class="ranking-table">
+                  <div class="ranking-table-header-row">
+                    <div class="ranking-table-header-data h6">Rank</div>
+                    <div class="ranking-table-header-data h6">Name</div>
+                    <div class="ranking-table-header-data h6">Plants</div>
+                  </div>
+                  <Row>
+                    <div class="ranking-table-row-leader-1">
+                      <div class="ranking-table-data-leader-1">
+                        <div class="medal-gold"></div>
+                      </div>
+                      <div class="ranking-table-data" id="name"></div>
+                      <div class="ranking-table-data">
+                        <div class="complete"></div>
+                      </div>
+                    </div>
+                  </Row>
+                  <div class="ranking-table-row-leader-2">
+                    <div class="ranking-table-data-leader-2">
+                      <div class="medal-silver"></div>
+                    </div>
+                    <div class="ranking-table-data">Raghav M</div>
+                    <div class="ranking-table-data">
+                      <div class="complete"></div>
+                    </div>
+                  </div>
+                  <div class="ranking-table-row-leader-3">
+                    <div class="ranking-table-data-leader-3">
+                      <div class="medal-bronze"></div>
+                    </div>
+                    <div class="ranking-table-data">Ryan Z</div>
+                    <div class="ranking-table-data">
+                      <div class="complete"></div>
+                    </div>
+                  </div>
+                  <div class="ranking-table-body">
+                    <div class="ranking-table-row">
+                      <div class="ranking-table-data">4</div>
+                      <div class="ranking-table-data">Joseph R</div>
+                      <div class="ranking-table-data">
+                        <div class="complete"></div>
+                      </div>
+                    </div>
+                    <div class="ranking-table-row">
+                      <div class="ranking-table-data">5</div>
+                      <div class="ranking-table-data">Muwaffaq I</div>
+                      <div class="ranking-table-data">
+                        <div class="complete"></div>
+                      </div>
+                    </div>
+                    <div class="ranking-table-row">
+                      <div class="ranking-table-data">6</div>
+                      <div class="ranking-table-data">Joshua L</div>
+                      <div class="ranking-table-data">
+                        <div class="complete"></div>
+                      </div>
+                    </div>
+                    <div class="ranking-table-row">
+                      <div class="ranking-table-data">7</div>
+                      <div class="ranking-table-data">Nick M</div>
+                      <div class="ranking-table-data">
+                        <div class="complete"></div>
+                      </div>
+                    </div>
+                    <div class="ranking-table-row">
+                      <div class="ranking-table-data">8</div>
+                      <div class="ranking-table-data">Ravindra R</div>
+                      <div class="ranking-table-data">
+                        <div class="complete"></div>
+                      </div>
+                    </div>
+                    <div class="ranking-table-row">
+                      <div class="ranking-table-data">9</div>
+                      <div class="ranking-table-data">Ricardo M</div>
+                      <div class="ranking-table-data">
+                        <div class="complete"></div>
+                      </div>
+                    </div>
+                    <div class="ranking-table-row">
+                      <div class="ranking-table-data">10</div>
+                      <div class="ranking-table-data">Shubham R</div>
+                      <div class="ranking-table-data">
+                        <div class="complete"></div>
+                      </div>
+                    </div>
+                    <div class="ranking-table-row">
+                      <div class="ranking-table-data">11</div>
+                      <div class="ranking-table-data">Flavio S</div>
+                      <div class="ranking-table-data">
+                        <div class="complete"></div>
+                      </div>
+                    </div>
+                    <div class="ranking-table-row">
+                      <div class="ranking-table-data">12</div>
+                      <div class="ranking-table-data">Igor S</div>
+                      <div class="ranking-table-data">
+                        <div class="complete"></div>
+                      </div>
+                    </div>
+                    <div class="ranking-table-row">
+                      <div class="ranking-table-data">13</div>
+                      <div class="ranking-table-data">Mycah H</div>
+                      <div class="ranking-table-data">
+                        <div class="complete"></div>
+                      </div>
+                    </div>
+                    <div class="ranking-table-row">
+                      <div class="ranking-table-data">14</div>
+                      <div class="ranking-table-data">hemant d</div>
+                      <div class="ranking-table-data">
+                        <div class="complete"></div>
+                      </div>
+                    </div>
+                    <div class="ranking-table-row">
+                      <div class="ranking-table-data">15</div>
+                      <div class="ranking-table-data">Santhosh S</div>
+                      <div class="ranking-table-data">
+                        <div class="complete"></div>
+                      </div>
+                    </div>
+                    <div class="ranking-table-row">
+                      <div class="ranking-table-data">16</div>
+                      <div class="ranking-table-data">Joseph D</div>
+                      <div class="ranking-table-data">
+                        <div class="complete"></div>
+                      </div>
+                    </div>
+                    <div class="ranking-table-row">
+                      <div class="ranking-table-data">17</div>
+                      <div class="ranking-table-data">Kevin C</div>
+                      <div class="ranking-table-data">
+                        <div class="complete"></div>
+                      </div>
+                    </div>
+                    <div class="ranking-table-row">
+                      <div class="ranking-table-data">18</div>
+                      <div class="ranking-table-data">yasser m</div>
+                      <div class="ranking-table-data">
+                        <div class="complete"></div>
+                      </div>
+                    </div>
+                    <div class="ranking-table-row">
+                      <div class="ranking-table-data">19</div>
+                      <div class="ranking-table-data">Miguel �ngel M</div>
+                      <div class="ranking-table-data">
+                        <div class="complete"></div>
+                      </div>
+                    </div>
+                    <div class="ranking-table-row">
+                      <div class="ranking-table-data">20</div>
+                      <div class="ranking-table-data">Peter K</div>
+                      <div class="ranking-table-data">
+                        <div class="complete"></div>
+                      </div>
+                    </div>
+                    <div class="ranking-table-row">
+                      <div class="ranking-table-data">21</div>
+                      <div class="ranking-table-data">Atikur R</div>
+                      <div class="ranking-table-data">
+                        <div class="complete"></div>
+                      </div>
+                    </div>
+                    <div class="ranking-table-row">
+                      <div class="ranking-table-data">22</div>
+                      <div class="ranking-table-data">Matthew Y</div>
+                      <div class="ranking-table-data">
+                        <div class="complete"></div>
+                      </div>
+                    </div>
+                    <div class="ranking-table-row">
+                      <div class="ranking-table-data">23</div>
+                      <div class="ranking-table-data">Anwar S</div>
+                      <div class="ranking-table-data">
+                        <div class="complete"></div>
+                      </div>
+                    </div>
+                    <div class="ranking-table-row">
+                      <div class="ranking-table-data">24</div>
+                      <div class="ranking-table-data">Jake Y</div>
+                      <div class="ranking-table-data">
+                        <div class="complete"></div>
+                      </div>
+                    </div>
+                    <div class="ranking-table-row">
+                      <div class="ranking-table-data">25</div>
+                      <div class="ranking-table-data">Lokesh R</div>
+                      <div class="ranking-table-data">
+                        <div class="complete"></div>
+                      </div>
+                    </div>
+                    <div class="ranking-table-row">
+                      <div class="ranking-table-data">26</div>
+                      <div class="ranking-table-data">Christopher P</div>
+                      <div class="ranking-table-data">
+                        <div class="complete"></div>
+                      </div>
+                    </div>
+                    <div class="ranking-table-row">
+                      <div class="ranking-table-data">27</div>
+                      <div class="ranking-table-data">Mingxin O</div>
+                      <div class="ranking-table-data">
+                        <div class="complete"></div>
+                      </div>
+                    </div>
+                    <div class="ranking-table-row">
+                      <div class="ranking-table-data">28</div>
+                      <div class="ranking-table-data">August E</div>
+                      <div class="ranking-table-data">
+                        <div class="complete"></div>
+                      </div>
+                    </div>
+                    <div class="ranking-table-row">
+                      <div class="ranking-table-data">29</div>
+                      <div class="ranking-table-data">Samsruti D</div>
+                      <div class="ranking-table-data">
+                        <div class="complete"></div>
+                      </div>
+                    </div>
+                    <div class="ranking-table-row">
+                      <div class="ranking-table-data">30</div>
+                      <div class="ranking-table-data">Harsh V</div>
+                      <div class="ranking-table-data">
+                        <div class="complete"></div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </body>
+        </div>
+      </Container>
+    );
   }
 };
 
