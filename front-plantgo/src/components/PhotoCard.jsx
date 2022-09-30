ed5504de145378b048b88d5c941e9f9ee9f63601
Inyoung.R <inyoung.r@gmail.com>
2022-09-30 12:40:34 +0900

    design: photocard design complete

--- front-plantgo/src/components/PhotoCard.jsx
+++ front-plantgo/src/components/PhotoCard.jsx
@@ -5,13 +5,22 @@ let PhotoCard = (props) => {
     return (
         <div className="photocard">
             <div style={{
-                backgroundImage: `url(${props.data.photoUrl})`,
-            }} className="front"></div>
-            <div className="back">
-                <p>{props.data.area}</p>
-                <p>{props.data.memo}</p>
+                backgroundImage: `URL("http://www.nature.go.kr/fileUpload/plants/basic/Polygonaceae/Persicaria/1314/1314_20160817152323712files.jpg")`
+                //backgroundImage: `url(${props.data.photoUrl})`,
+            }} className="front">
+                <p>2022.03.04</p>
+                <br></br>
+                <p>강남구</p>
             </div>
-            <button className="photocardbutton">카카오톡 공유</button>
+            <div className="back">
+                <div>
+                    <p>강남구에서 찾았다!! 나도 곧 식물박사!!</p>
+                    {/* <p>{props.data.area}</p>
+                    <p>{props.data.memo}</p> */}
+
+                </div>
+                <button className="photocardbutton">Memo</button>
+            </div>
         </div>
     )
 }
